02d0509e5ac13ff40939367848a298660eeb9044
jose.padin <jose.padin@edition1.nl>
2021-01-19 10:16:27 +0100

    Signup and create/delete to-do
    * Implemented feature to register users
    * Implemented methods to create and delete todos

--- app/firestore_service.py
+++ app/firestore_service.py
@@ -8,13 +8,28 @@ firebase_admin.initialize_app(credential)
 db = firestore.client()
 
 
-def get_users():
-    return db.collection('users').get()
-
-
 def get_user(user_id):
     return db.collection('users').document(user_id).get()
 
 
+def get_users():
+    return db.collection('users').get()
+
+
+def user_put(user_data):
+    user_ref = db.collection('users').document(user_data.username)
+    user_ref.set({'password': user_data.password})
+
+
 def get_todos(user_id):
     return db.collection('users').document(user_id).collection('todos').get()
+
+
+def create_todo(user_id, description):
+    todo_collection_ref = db.collection('users').document(user_id).collection('todos')
+    todo_collection_ref.document().set({'description': description, 'done': False})
+
+
+def delete_todo(user_id, todo_id):
+    todo_ref = db.collection('users').document(user_id).collection('todos').document(todo_id)
+    todo_ref.delete()
